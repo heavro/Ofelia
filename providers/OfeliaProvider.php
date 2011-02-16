6b88d42cb848c2fc579f53bea8c389dd3a87a101
Renich Bon Ciric <renich@woralelandia.com>
2011-02-16 02:23:45 -0600

    added proper output mechanism

--- providers/OfeliaProvider.php
+++ providers/OfeliaProvider.php
@@ -8,12 +8,16 @@ class OfeliaProvider extends Zend_Tool_Project_Provider_Abstract
 
     public function install()
     {
-        echo 'This is not implemented yet.';
+        $this->_registry
+             ->getResponse()
+             ->appendContent('This is not implemented yet.');
     }
 
     public function uninstall()
     {
-        echo 'This is not implemented yet.';
+        $this->_registry
+             ->getResponse()
+             ->appendContent('This is not implemented yet.');
     }
 
 
