3b7cf870098fda004d97bcca1da2299aac573ccd
Renich Bon Ciric <renich@woralelandia.com>
2011-02-16 02:19:02 -0600

    provider creation and actions added with a disabled message

---
+++ providers/OfeliaProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once 'Zend/Tool/Project/Provider/Abstract.php';
+require_once 'Zend/Tool/Project/Provider/Exception.php';
+
+class OfeliaProvider extends Zend_Tool_Project_Provider_Abstract
+{
+
+    public function install()
+    {
+        echo 'This is not implemented yet.';
+    }
+
+    public function uninstall()
+    {
+        echo 'This is not implemented yet.';
+    }
+
+
+}
