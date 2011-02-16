c8c7fc09fcbe628fef115d10a2e8ee9fee54ac49
Renich Bon Ciric <renich@woralelandia.com>
2011-02-16 06:48:39 -0600

    added a form, some validation and filtering and a redirect
    still need to work on it and integrate it with auth

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -7,15 +7,33 @@ class UserController extends Zend_Controller_Action
     {
         /* Initialize action controller here */
     }
+    
+    public function getForm()
+    {
+        $form = new Application_Form_Login;
+        return $form;
+    }
 
     public function indexAction()
     {
-        // action body
+        $form = $this->getForm();
+        $this->view->form = $form;
     }
 
     public function loginAction()
     {
-        // action body
+        if (!$this->getRequest()->isPost()) {
+            return $this->_forward('index');
+        }
+        $form = $this->getForm();
+        if (!$form->isValid($_POST)) {
+            // Failed validation; redisplay form
+            $this->view->form = $form;
+            $this->render('index');
+        }
+ 
+        $this->view->values = $form->getValues();
+        // now try and authenticate....
     }
 
 
--- application/forms/Login.php
+++ application/forms/Login.php
@@ -2,12 +2,30 @@
 
 class Application_Form_Login extends Zend_Form
 {
-
     public function init()
     {
-        /* Form Elements & Other Definitions Here ... */
+        $this->setAction('/user/login')
+             ->setMethod('post')
+             ->setName('login');
+         
+        // Create and configure username element:
+        $username = $this->createElement('text', 'username');
+        $username->setLabel('Username')
+                 ->addValidator('alnum')
+                 ->addValidator('regex', false, array('/^[a-z]+/'))
+                 ->addValidator('stringLength', false, array(6, 20))
+                 ->setRequired(true)
+                 ->addFilter('StringToLower');
+         
+        // Create and configure password element:
+        $password = $this->createElement('password', 'password');
+        $password->setLabel('Password')
+                 ->addValidator('StringLength', false, array(6))
+                 ->setRequired(true);
+         
+        // Add elements to form:
+        $this->addElement($username)
+             ->addElement($password)
+             ->addElement('submit', 'login', array('label' => 'Login'));
     }
-
-
 }
-
