a4c92565cb7a6373d5df284ccc453014bda53472
Renich Bon Ciric <renich@woralelandia.com>
2011-02-21 05:47:35 -0600

    sqlite scripts tested and working

--- scripts/users-example-data.sqlite.sql
+++ scripts/users-example-data.sqlite.sql
@@ -1,6 +1,12 @@
 -- Renich Bon Ciric - u:renich, p:password
-INSERT INTO users VALUES(
-    'null',
+INSERT INTO users(
+    'username',
+    'password',
+    'email',
+    'name',
+    'authenticated',
+    'disabled'
+) VALUES(
     'renich',
     'c8fed00eb2e87f1cee8e90ebbe870c190ac3848c',
     'renich@woralelandia.com',
@@ -9,24 +15,36 @@ INSERT INTO users VALUES(
     'false'
 );
 
--- Francisco Villa - u:pancho, p:password
-INSERT INTO users VALUES(
-    'null',
-    'pancho',
-    'c8fed00eb2e87f1cee8e90ebbe870c190ac3848c',
-    'pancho@woralelandia.com',
-    'Francisco Villa',
-    'false',
-    'false'
-);
-
--- 
-INSERT INTO users VALUES(
-    'null',
+-- Miguel Hidalgo y Costilla - u:miguel, p:password
+INSERT INTO users(
+    'username',
+    'password',
+    'email',
+    'name',
+    'authenticated',
+    'disabled'
+) VALUES(
     'miguel',
     'c8fed00eb2e87f1cee8e90ebbe870c190ac3848c',
     'miguel@woralelandia.com',
     'Miguel Hidalgo y Costilla',
+    'true',
+    'false'
+);
+
+-- Francisco Villa - u:pancho, p:password
+INSERT INTO users(
+    'username',
+    'password',
+    'email',
+    'name',
+    'authenticated',
+    'disabled'
+) VALUES(
+    'pancho',
+    'c8fed00eb2e87f1cee8e90ebbe870c190ac3848c',
+    'pancho@woralelandia.com',
+    'Francisco Villa',
     'false',
     'false'
 );
