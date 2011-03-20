7a5a5feaa52fa9033700016ec45581eddf9941bb
Renich Bon Ciric <renich@woralelandia.com>
2011-03-19 20:46:12 -0600

    agregué fixperms

--- providers/OfeliaProvider.php
+++ providers/OfeliaProvider.php
@@ -5,17 +5,19 @@ require_once 'Zend/Tool/Project/Provider/Exception.php';
 
 class OfeliaProvider extends Zend_Tool_Project_Provider_Abstract
 {
+
     public function install()
     {
-        $this->_registry
-             ->getResponse()
-             ->appendContent('This is not implemented yet.');
+        /** @todo Implementation */
     }
 
     public function uninstall()
     {
-        $this->_registry
-             ->getResponse()
-             ->appendContent('This is not implemented yet.');
+        /** @todo Implementation */
     }
+
+    public function fixperms()
+    {
+
+	}
 }
